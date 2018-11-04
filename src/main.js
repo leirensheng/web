3dfc3d812780e9014389cf9024e7fdb1954cdde1
leirensheng <leirensheng@163.com>
2018-11-04 14:53:08 +0800

    feat:去掉ele

--- src/main.js
+++ src/main.js
@@ -1,11 +1,11 @@
 import Vue from "vue";
 import App from "./App.vue";
-import ElementUI from "element-ui";
+// import ElementUI from "element-ui";
 import router from "./router";  // router保持小写，大写会出错误
 import "./assets/iconfont/iconfont.css";
 import "element-ui/lib/theme-chalk/index.css";
 Vue.config.productionTip = false;
-Vue.use(ElementUI); // , { size: 'small' }
+// Vue.use(ElementUI); // , { size: 'small' }
 
 new Vue({
     router,
