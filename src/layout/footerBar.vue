bce9e03526b7daca580383af32fb0a64ff458d39
leirensheng <leirensheng@163.com>
2018-11-05 21:39:35 +0800

    feat:去掉底部大部分内容

--- src/layout/footerBar.vue
+++ src/layout/footerBar.vue
@@ -1,8 +1,8 @@
 <template>
   <div id="footer">
-    <div id='footerKeywordContainer'>
+    <!-- <div id='footerKeywordContainer'>
       <div id='footerKeyword' v-for="(one,index) in keywords" :key='index'>{{one.keyword}}</div>
-    </div>
+    </div> -->
     <div id='copyright'>Copyright © 2018 优设</div>
   </div>
 </template>
@@ -32,7 +32,7 @@ $orange: rgb(255, 90, 0);
 #footer {
   padding: 0.9rem;
   font-size:0.9rem; 
-  height: 4rem;
+  // height: 4rem;
   background-color: rgb(34, 31, 31);
   // color: rgb(170,170,170);
   // width: 100%;
@@ -45,7 +45,7 @@ $orange: rgb(255, 90, 0);
   }
   #copyright{
     text-align: center;
-    margin-top: 1rem;
+    // margin-top: 1rem;
   }
 }
 </style>
