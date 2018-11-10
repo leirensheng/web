2ec478e96bcac34fb90a61b98de8a581ffc812b6
leirensheng <leirensheng@163.com>
2018-11-10 16:54:00 +0800

    feat:尝试预加载

--- src/main.js
+++ src/main.js
@@ -9,5 +9,8 @@ Vue.config.productionTip = false;
 
 new Vue({
     router,
-    render: h => h(App)
+    render: h => h(App),
+    mounted () {
+        document.dispatchEvent(new Event("render-event"));
+    }
 }).$mount("#app");
