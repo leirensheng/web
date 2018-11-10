a4db2374503ab06b2d0205926dd46a761458c23a
leirensheng <leirensheng@163.com>
2018-11-10 16:59:17 +0800

    feat:修改目录

--- vue.config.js
+++ vue.config.js
@@ -14,7 +14,7 @@ module.exports = {
                         // 生成文件的路径，也可以与webpakc打包的一致。
                         // 下面这句话非常重要！！！
                         // 这个目录只能有一级，如果目录层次大于一级，在生成的时候不会有任何错误提示，在预渲染的时候只会卡着不动。
-                        staticDir: path.join(__dirname, "../dist"),
+                        staticDir: path.join(__dirname, "/dist"),
                       
                         // 对应自己的路由文件，比如a有参数，就需要写成 /a/param1。
                         routes: ["/", "/home"],
