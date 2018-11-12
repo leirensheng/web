fefc5b93bb87ca35f87c2f8a295e39fb4e3a8923
leirensheng <leirensheng@163.com>
2018-11-12 21:11:34 +0800

    feat:漏了

--- vue.config.js
+++ vue.config.js
@@ -1,6 +1,6 @@
-const PrerenderSPAPlugin = require("prerender-spa-plugin");
-const Renderer = PrerenderSPAPlugin.PuppeteerRenderer;
-const path= require("path");
+// const PrerenderSPAPlugin = require("prerender-spa-plugin");
+// const Renderer = PrerenderSPAPlugin.PuppeteerRenderer;
+// const path= require("path");
 module.exports = {
     // 选项...
     // baseUrl: process.env.NODE_ENV === "production"
