e65c27b38656f4a54a4e2ac5a4032bcb27aa3b28
leirensheng <leirensheng@163.com>
2018-11-12 21:10:22 +0800

    feat:先去掉预渲染

--- vue.config.js
+++ vue.config.js
@@ -6,42 +6,42 @@ module.exports = {
     // baseUrl: process.env.NODE_ENV === "production"
     //     ? "/public/"
     //     : "/"
-    configureWebpack: config=>{
-        if(process.env.NODE_ENV==="production"){
-            return {
-                plugins: [
-                    new PrerenderSPAPlugin({
-                        // 生成文件的路径，也可以与webpakc打包的一致。
-                        // 下面这句话非常重要！！！
-                        // 这个目录只能有一级，如果目录层次大于一级，在生成的时候不会有任何错误提示，在预渲染的时候只会卡着不动。
-                        staticDir: path.join(__dirname, "/dist"),
+    // configureWebpack: config=>{
+    //     if(process.env.NODE_ENV==="production"){
+    //         return {
+    //             plugins: [
+    //                 new PrerenderSPAPlugin({
+    //                     // 生成文件的路径，也可以与webpakc打包的一致。
+    //                     // 下面这句话非常重要！！！
+    //                     // 这个目录只能有一级，如果目录层次大于一级，在生成的时候不会有任何错误提示，在预渲染的时候只会卡着不动。
+    //                     staticDir: path.join(__dirname, "/dist"),
                       
-                        // 对应自己的路由文件，比如a有参数，就需要写成 /a/param1。
-                        routes: ["/home"],
+    //                     // 对应自己的路由文件，比如a有参数，就需要写成 /a/param1。
+    //                     routes: ["/home"],
                       
-                        // 预渲染代理接口
-                        server: {
-                            proxy: {
-                                "/getNews": {
-                                    target: "http://localhost:7001",
-                                    secure: false
-                                }
-                            }
-                        },
-                        //在一定时间后再捕获页面信息，使得页面数据信息加载完成
-                        captureAfterTime: 30000,
-                        // 这个很重要，如果没有配置这段，也不会进行预编译
-                        renderer: new Renderer({
-                            inject: {
-                                foo: "bar"
-                            },
-                            headless: false,
-                            // 在 main.js 中 document.dispatchEvent(new Event('render-event'))，两者的事件名称要对应上。
-                            renderAfterDocumentEvent: "render-event"
-                        })
-                    }),        ]
-            };
-        }
-    },
+    //                     // 预渲染代理接口
+    //                     server: {
+    //                         proxy: {
+    //                             "/getNews": {
+    //                                 target: "http://localhost:7001",
+    //                                 secure: false
+    //                             }
+    //                         }
+    //                     },
+    //                     //在一定时间后再捕获页面信息，使得页面数据信息加载完成
+    //                     captureAfterTime: 30000,
+    //                     // 这个很重要，如果没有配置这段，也不会进行预编译
+    //                     renderer: new Renderer({
+    //                         inject: {
+    //                             foo: "bar"
+    //                         },
+    //                         headless: false,
+    //                         // 在 main.js 中 document.dispatchEvent(new Event('render-event'))，两者的事件名称要对应上。
+    //                         renderAfterDocumentEvent: "render-event"
+    //                     })
+    //                 }),        ]
+    //         };
+    //     }
+    // },
     baseUrl:  "/"
 };
